461303cb081b7dac660eb25630f9f1a9aae7b166
CarstenVilstrup <carstenvilstrup@hotmail.com>
2019-02-18 13:40:19 +0100

    Revert "8th commit"
    This reverts commit 171f73ec26ca59bc6d1455ca545a40503dfd4233.

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,15 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<div class="page-banner">
-			<div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg);"></div>
-				<div class="page-banner__content container t-center c-white">
-				<h1 class="headline headline--large">Welcome!</h1>
-				<h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-				<h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-				<a href="#" class="btn btn--large btn--blue">Find Your Major</a>
-			</div>
-		</div>
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
