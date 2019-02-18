104374b82797b6a5e3c8091068d54d94035fb089
CarstenVilstrup <carstenvilstrup@hotmail.com>
2019-02-18 15:03:46 +0100

    klar header, css saknas

--- header.php
+++ header.php
@@ -19,8 +19,8 @@
 
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cvstart' ); ?></a>
 
@@ -31,13 +31,13 @@
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
 					?>
-					<h1 class="school-logo-text float-left"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><strong>Fictional</strong> University</a></h1>
+					<h1 class="school-logo-text float-left"><a href="<?php echo esc_url( home_url( 'home' ) ); ?>"><strong>Fictional</strong> University</a></h1>
 					<span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
 					<i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
     			<?php
 				else :
 					?>
-					<h1 class="school-logo-text float-left"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><strong>Fictional</strong> University</a></h1>
+					<h1 class="school-logo-text float-left"><a href="<?php echo esc_url( home_url( 'home' ) ); ?>"><strong>Fictional</strong> University</a></h1>
 					<span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
 					<i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
 					
@@ -50,16 +50,31 @@
 				<?php endif; ?>
 			</div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cvstart' ); ?></button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</nav><!-- #site-navigation -->
-		</div>		<!-- container -->
+			<div class="site-header__menu group">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cvstart' ); ?></button>
+					<ul>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Programs</a></li>
+							<li><a href="#">Events</a></li>
+							<li><a href="#">Campuses</a></li>
+							<li><a href="#">Blog</a></li>
+						</ul>
+					
+					<!-- <?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					) );
+					?> -->
+				</nav><!-- #site-navigation -->
+				<div class="site-header__util">
+          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+          <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        </div>
+			</div>	<!-- .site-header__menu group -->
+		</div>		<!-- .container -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
